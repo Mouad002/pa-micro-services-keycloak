dddf96431d80aef5bbec550be6bfb064237aab84
mouad002 <elcosto02@gmail.com>
2025-12-25 17:04:16 +0100

    project initialization

---
+++ src/main/java/org/example/pa4microserviceskeyclock/Pa4MicroServicesKeyclockApplication.java
@@ -0,0 +1,13 @@
+package org.example.pa4microserviceskeyclock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa4MicroServicesKeyclockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pa4MicroServicesKeyclockApplication.class, args);
+    }
+
+}
